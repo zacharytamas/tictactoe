492810b875ab9c471816eaae7873574856398e4f
Zachary Jones <zacharytamas@gmail.com>
2014-01-26 17:08:32 -0600

    Late start today because it's the weekend. Simplify the is_win_state function to be more succinct. Before was kind of a clumsy way of representing the state.

--- strategy.py
+++ strategy.py
@@ -13,39 +13,40 @@ def is_win_state(board_state):
     Returns whether the current board state is a winning state.
 
     Output: Tuple of 3 values.
-    (<whether is win or not>, <who won, if applicable>, <what winning state was met>)
+    Tuple of two values
+    (<X|O|TIE|None>, <mask of winning state that was met>)
 
     # Board is empty
     >>> is_win_state([None] * 9)
-    (False, None, None)
+    (None, None)
 
     # Horizontal wins
     >>> is_win_state(['X', 'X', 'X'] + [None] * 6)
-    (True, 'X', 7)
+    ('X', 7)
     >>> is_win_state([None] * 3 + ['X', 'X', 'X'] + [None] * 3)
-    (True, 'X', 56)
+    ('X', 56)
     >>> is_win_state([None] * 6 + ['X', 'X', 'X'])
-    (True, 'X', 448)
+    ('X', 448)
 
     # Vertical wins
     >>> is_win_state(['O', None, None] * 3)
-    (True, 'O', 73)
+    ('O', 73)
     >>> is_win_state([None, 'O', None] * 3)
-    (True, 'O', 146)
+    ('O', 146)
     >>> is_win_state([None, None, 'O'] * 3)
-    (True, 'O', 292)
+    ('O', 292)
 
     # Diagonal wins
     >>> is_win_state(['X', None, None, None] * 3)
-    (True, 'X', 273)
+    ('X', 273)
     >>> is_win_state([None, None, 'X', None, 'X', None, 'X', None, None])
-    (True, 'X', 84)
+    ('X', 84)
 
     # Ties
     >>> is_win_state(list('XOOOXXOXO'))
-    (False, 'TIE', None)
+    ('TIE', None)
     >>> is_win_state(list('OOXXXOOXO'))
-    (False, 'TIE', None)
+    ('TIE', None)
     """
 
     masks = { 'X': 0, 'O': 0 }
