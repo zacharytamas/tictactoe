140c3a3923cb9775a651da9ec9f04d67d4fc933f
Zachary Jones <zacharytamas@gmail.com>
2014-01-27 00:08:00 -0600

    Have to stop for the night (weekend is over) but have most of the gameplay mechanics in place. Mostly just need to work on the UX but that's no problem. Looking good! (well not really, it's hideous, but functionally it's looking good :P)

--- serve.py
+++ serve.py
@@ -1,10 +1,39 @@
 
-from flask import Flask, render_template
+from flask import (Flask, render_template, request, jsonify)
+
+from strategy import create_board, computer_play, is_win_state
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return render_template('index.html')
+
+
+@app.route('/game', methods=['GET', 'POST'])
+def game():
+    """A REST adaptation discusses state of a
+    Tic-Tac-Toe game with an external player."""
+
+    # A `GET` to the API simply returns a new, blank game.
+    if request.method == 'GET':
+        return jsonify(board_state=create_board(),
+                       win=(False, None))
+
+    # If API is accessed with a `POST`, it is the external
+    # player submitting his move.
+    elif request.method == 'POST':
+        board_state = request.json['board_state']
+
+        # Check if the human has won or tied the game.
+        win = is_win_state(board_state)
+        if win[0]:
+            return jsonify(board_state=board_state, win=win)
+        else:
+            board_state[computer_play(board_state)] = 'O'
+            return jsonify(board_state=board_state, win=is_win_state(board_state))
+
 
 if __name__ == "__main__":
 
--- static/js/TicTac.js
+++ static/js/TicTac.js
@@ -8,9 +8,97 @@
   ////////////////////////////////////////////////
 
   TT.BoardModel = Backbone.Model.extend({
-    url: '/game'
+    url: '/game',
+
+    markSquare: function (square) {
+      var state = this.toJSON().board_state;
+
+      if (state[square] !== null) {
+        return false;
+      }
+
+      state[square] = 'X';
+      this.set({board_state: state});
+
+      return true;
+    }
+
   });
 
+  ////////////////////////////////////////////////
+  // Views
+  ////////////////////////////////////////////////
+
+  TT.BoardView = Backbone.View.extend({
+    el: '#game-board',  // This already exists on the page.
+
+    events: {
+      'click td': 'squareWasClicked'
+    },
+
+    initialize: function () {
+      this.listenTo(TT.boardModel, 'change', this.renderUpdates);
+    },
+
+    squareWasClicked: function (event) {
+      var square = parseInt($(event.target).attr('id').split('-')[1], 10);
+      var marked = TT.boardModel.markSquare(square);
+
+      if (marked) {
+        TT.boardModel.save();
+      } else {
+        alert('invalid choice');
+      }
+    },
+
+    render: function () {
+      return this.renderUpdates();
+    },
+
+    renderUpdates: function () {
+
+      var win = TT.boardModel.get('win');
+
+      if (win && win[0]) {
+
+        // Highlight the squares responsible for the win.
+
+        for (var i = 0; i < 9; i++) {
+          if (win[0] === 'TIE') {
+            break;
+          }
+
+          if ((1 << i) & win[1]) {
+            $('#square-' + i).addClass('winning');
+          }
+        }
+
+
+      } else {
+        this.$('td').removeClass('winning');
+      }
+
+      return this.renderSquares();
+    },
+
+    renderSquares: function () {
+      this.$('td').empty();
+
+      _.each(TT.boardModel.get('board_state'), function (value, i) {
+        $('#square-' + i).text(value || '');
+      });
+
+      return this;
+    }
+  });
+
+  // Setup data
   TT.boardModel = new TT.BoardModel();
+
+  // Setup the interface
+  TT.boardView = new TT.BoardView().render();
+
+  // Fetch initial game state.
+  TT.boardModel.fetch();
 
 }());
